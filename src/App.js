d5a17f41a8592288511485f44a9b5ea3a1be47c5
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-10 15:32:34 -0500

    Added Route

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import { Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms/" component={Rooms} />
+      <Route exact path="/rooms/:slug" component={SingleRoom} />
     </div>
   );
 }
