f01d49356934c5203913bb071df2ad8a81510117
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-15 16:28:24 -0500

    added background image to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
 
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
 
 export default function Home() {
     return (
-        <div>
-            hello from home page
-        </div>
+        <Hero />
     )
 }
