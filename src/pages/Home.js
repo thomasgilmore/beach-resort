2147c22b627912a0559b6c244c87f143d9ffb27b
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-17 18:52:38 -0500

    added Services to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,11 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
+import Services from '../components/Services';
 
 export default function Home() {
     return (
+        <>
         <Hero> 
             <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
                 <Link to="/rooms" className="btn-primary">
@@ -13,5 +15,7 @@ export default function Home() {
                 </Link>
             </Banner>
         </Hero>
+        <Services />
+        </>
     )
 }
