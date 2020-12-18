953894837222bc522aafa7e0c8fd7eaaf755c87b
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-18 17:01:20 -0500

    imported FeaturedRooms

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import Services from '../components/Services';
+import FeaturedRooms from '../components/FeaturedRooms';
 
 export default function Home() {
     return (
@@ -16,6 +17,7 @@ export default function Home() {
             </Banner>
         </Hero>
         <Services />
+        <FeaturedRooms />
         </>
     )
 }
