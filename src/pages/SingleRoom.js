aabd0c5f91ed8e703b871c2e485abe372522592f
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-22 13:29:31 -0500

    Working on SingleRoom.js

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -8,16 +8,32 @@ import { RoomContext } from '../context';
 export default class SingleRoom extends Component {
     constructor(props) {
         super(props)
-        console.log(this.props);
+        // console.log(this.props);
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg 
+        };
     }
-    componentDidMount(){
-
-    }
+    static contextType = RoomContext;
+    // componentDidMount(){}
     render() {
-        return (
-            <div>
-                hello from single room page
+        const { getRoom } = this.context;
+        const room = getRoom(this.state.slug);
+        if(!room) {
+            return <div className="error">
+                <h3>no such room could be found...</h3>
+                <Link to='/rooms' className="btn-primary">back to rooms</Link>
             </div>
+        }
+        const { name, description, capacity, size, price, extras, breakfast, pets, images } = room;
+        return (
+            <Hero hero='roomsHero'>
+                <Banner title={`${name} room`}>
+                    <Link to='/rooms' className='btn-primary'>
+                        back to rooms
+                    </Link>
+                </Banner>
+            </Hero>
         )
     }
 }
