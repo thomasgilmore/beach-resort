f06ea2704ea965ddcb68d75c03de08d14eaa5a09
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-23 16:20:18 -0500

    working on project

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import RoomsFilter from './RoomFilter';
 import RoomsList from './RoomList';
+import { RoomConsumer } from '../context';
+import Loading from './Loading';
 
 export default function RoomContainer() {
     return (
-        <div>
-            Hello From Rooms Container
-            <RoomsFilter />
-            <RoomsList />
-        </div>
+        <RoomConsumer>
+        {(value) => {
+                console.log(value);
+                return (
+                    <div>
+                        Hello From Rooms Container
+                        <RoomsFilter />
+                        <RoomsList />
+                    </div>
+                )
+            }
+        }   
+        </RoomConsumer>
     )
 }
