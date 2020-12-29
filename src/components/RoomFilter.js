167161ec924c613971ab1a63b09bb6a79038f1a6
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-29 16:39:10 -0500

    working on RoomFilter.js

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { RoomContext } from '../context';
+import Title from '../components/Title';
 
 export default function RoomFilter() {
+    const context = useContext(RoomContext);
+    const { 
+        handleChange, 
+        type, 
+        capacity, 
+        price, 
+        minPrice, 
+        maxPrice, 
+        minSize, 
+        maxSize, 
+        breakfast, 
+        pets 
+    }  = context;
+
     return (
-        <div>
-            hello from room filter
-        </div>
+        <section className="filter-container">
+            <Title title="search rooms" />
+            <form className="filter-form">
+                {/* select type */}
+                <div className="form-group">
+                    <label htmlFor="type">room type</label>
+                    <select name="type" id=""></select>
+                </div>
+                {/* end select type */}
+            </form>
+        </section>
     )
 }
