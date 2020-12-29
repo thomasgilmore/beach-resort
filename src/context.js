64752aa4c1d1093ce73361a4508aa5a81bc39832
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-29 16:40:30 -0500

    working on context.js

--- src/context.js
+++ src/context.js
@@ -8,7 +8,16 @@ class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: true
+        loading: true,
+        type: 'all',
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        breakfast: false,
+        pets: false
     };
     // getData
 
@@ -16,11 +25,16 @@ class RoomProvider extends Component {
         // this.getData
         let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
         this.setState({
             rooms,
             featuredRooms,
             sortedRooms: rooms,
-            loading: false
+            loading: false,
+            price: maxPrice,
+            maxPrice,
+            maxSize
         })
     }
 
@@ -41,9 +55,25 @@ class RoomProvider extends Component {
         return room;
     }
 
+    handleChange = event => {
+        const type = event.target.type;
+        const name = event.target.name;
+        const value = event.target.value;
+        console.log(type, name, value);
+
+    }
+
+    filterRooms = () => {
+        console.log("hello");
+    }
+
     render() {
         return (
-            <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+            <RoomContext.Provider value={{ 
+                ...this.state, getRoom: this.getRoom, 
+                handleChange: this.handleChange 
+            }}
+            >
                 {this.props.children}
             </RoomContext.Provider>
         )
