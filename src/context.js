8d91ff14f04eea92628570ba9ed75e4e9b4e9320
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-30 18:52:32 -0500

    added filter by capacity to context.js

--- src/context.js
+++ src/context.js
@@ -69,10 +69,21 @@ class RoomProvider extends Component {
             rooms, type, capacity, price, minSize, maxSize, breakfast, pets
         } = this.state
 
+        // all the rooms
         let tempRooms = [...rooms];
+        // transform value
+        capacity = parseInt(capacity)
+
+        // filter by type
         if(type !== 'all') {
             tempRooms = tempRooms.filter(room => room.type === type)
         }
+
+        // filter by capacity
+        if(capacity !== 1) {
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+        }
+
         this.setState({
             sortedRooms: tempRooms
         })
