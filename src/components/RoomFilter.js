d2c5903254e83c4c3fffeb0747a26a0c030268cd
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-12-31 11:21:58 -0500

    added price range to project

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -68,7 +68,13 @@ export default function RoomFilter({rooms}) {
                         {people}
                     </select>
                 </div>
-                {/* end select type */}
+                {/* end guests */}
+                {/* room price */}
+                <div className="form-group">
+                    <label htmlFor="price">room price ${price}</label>
+                    <input type="range" name="price" min={minPrice} max={maxPrice} id="price" value={price} onChange={handleChange} className="form-control" />
+                </div>
+                {/* end of room price */}
             </form>
         </section>
     )
