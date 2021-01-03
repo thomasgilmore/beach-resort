77c27425ffebb878eb25eeaf53b6c22a6334e3a6
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-01-03 15:00:08 -0500

    working on project

--- src/context.js
+++ src/context.js
@@ -57,8 +57,8 @@ class RoomProvider extends Component {
 
     handleChange = event => {
         const target = event.target;
-        const value = event.type === 'checkbox' ? target.checked : target.value;
-        const name = event.target.name;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
         this.setState({
             [name]: value
         }, this.filterRooms)
