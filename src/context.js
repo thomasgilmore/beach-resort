d868086ea58fb35f6fa94c5dfe5e46d40f0c9058
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-01-03 14:55:46 -0500

    added room size filter to project:

--- src/context.js
+++ src/context.js
@@ -86,6 +86,9 @@ class RoomProvider extends Component {
         }
         // filter by price
         tempRooms = tempRooms.filter(room => room.price <= price);
+        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize);
+        
+
 
         // change state
         this.setState({
